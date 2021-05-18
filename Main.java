7bb8d6e26eb7f90f9b7016f922195228946f1694
Tyurina Olga <tyurinaolga19@gmail.com>
2021-05-18 09:16:51 +0300

    aded files

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+        int bonus = 20; //miles 
+        int result = ticketPrice / bonus;
+        System.out.println(result);
+
+
+    }
+}
